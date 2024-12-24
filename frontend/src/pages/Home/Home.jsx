734c1700d3999f5f20eb88725f2e2408e44277a0
YASH24824 <pyash7452@gmail.com>
2024-12-24 17:48:56 +0530

    add sechdule

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,25 +1,27 @@
-import React from 'react'
-import Navbar from '../../components/Navbar/Navbar'
-import Carousel from '../../components/carousel/Carousel'
-import Hero from '../../components/Hero/Hero'
-import Gallery from '../../components/PhotoGallery/Gallery'
-import Footer from '../../components/Footer/Footer'
-import How from '../../components/How/How'
-import Aboutus from '../../pages/Aboutus/About'
-import Faq from '../../components/FAQ/Faq'
+import React from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import Carousel from "../../components/carousel/Carousel";
+import Hero from "../../components/Hero/Hero";
+import Gallery from "../../components/PhotoGallery/Gallery";
+import Footer from "../../components/Footer/Footer";
+import How from "../../components/How/How";
+import Aboutus from "../../pages/Aboutus/About";
+import Faq from "../../components/FAQ/Faq";
+import Schedule from "../../components/Sechdule/Sechdule";
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Hero />
-      <How/>
-      <Aboutus/>
+      <How />
+      <Aboutus />
       {/* <Carousel /> */}
       <Gallery />
-      <Faq/>
+      <Schedule />
+      <Faq />
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
