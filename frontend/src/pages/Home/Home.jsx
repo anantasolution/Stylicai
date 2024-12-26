cfb12cb2dbf5be989e2b12477a7738ef4a879e00
YASH24824 <pyash7452@gmail.com>
2024-12-26 17:27:13 +0530

    brandcarousel

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -9,19 +9,24 @@ import Faq from "../../components/FAQ/Faq";
 import Schedule from "../../components/Sechdule/Sechdule"; // Kept 'Schedule' from main
 import Aboutus from "../../components/Aboutus/Aboutus";
 import Testimonial from "../../components/Testimonial/Testimonial"
+import Brandingcarousel from "../../components/brandingcarousel/brandingcarousel";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+    
       <Hero />
       <How />
       <Aboutus />
       <Carousel />
       <Gallery />
+      <Brandingcarousel/>
       <Faq/>
       <Schedule></Schedule>
       <Testimonial />
+      
+   
       <Footer />
     </div>
   );
